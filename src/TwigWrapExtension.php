109a1a1ce227e726d49d102279f9a746e84f37a9
Thomas Helmrich <thomas@gigabit.de>
2018-01-31 18:04:52 +0100

    code format

--- src/TwigWrapExtension.php
+++ src/TwigWrapExtension.php
@@ -9,14 +9,12 @@ use Bolt\Extension\SimpleExtension;
  *
  * @author Thomas Helmrich <thomas@gigabit.de>
  */
-class TwigWrapExtension extends SimpleExtension
-{
+class TwigWrapExtension extends SimpleExtension {
 
     /**
      * {@inheritdoc}
      */
-    protected function registerTwigFilters()
-    {
+    protected function registerTwigFilters() {
         return [
             'textWrap' => 'textWrapFilter',
         ];
@@ -25,12 +23,11 @@ class TwigWrapExtension extends SimpleExtension
     /**
      * {@inheritdoc}
      */
-    protected function registerTwigPaths()
-    {
+    protected function registerTwigPaths() {
         return [
             'templates/normal',
-            'templates/other'   => ['namespace' => 'TwigWrap'],
-            'templates/special' => ['namespace' => 'Gigabit', 'position' => 'prepend']
+            'templates/other' => ['namespace' => 'TwigWrap'],
+            'templates/special' => ['namespace' => 'Gigabit', 'position' => 'prepend'],
         ];
     }
 
@@ -38,18 +35,57 @@ class TwigWrapExtension extends SimpleExtension
      * Wraps a text that matches the keyword with the given strings
      *
      * @param string $input
-     * @param string $keyWord
-     * @param string $prepend
-     * @param string $append
+     * @param string $keyword
+     * @param string $prefix
+     * @param string $affix
      *
      * @return string
      */
-    public function textWrapFilter($input, $keyWord, $prepend, $append)
-    {
+    public function textWrapFilter($input, $keyword, $prefix = '', $affix = '') {
+        mb_internal_encoding("UTF-8");
 
+        $keywordPos = mb_stripos($input, $keyword);
+        if ($keywordPos === false) {
+            return $input;
+        }
 
+        $inputLength = mb_strlen($input);
 
-        return strtolower($input);
+        $inputStart = mb_substr($input, 0, $keywordPos);
+
+        $startingSpacePos = mb_strripos($inputStart, ' ');
+        if ($startingSpacePos === false) {
+            $startingSpacePos = -1;
+        }
+        $matchingWord = mb_substr($input, $startingSpacePos+1, $inputLength);
+
+        $endingSpacePos = $this->stripos_array($matchingWord, array(' ', ',', '.', '!', '?', ';', '""'));
+        if ($endingSpacePos === false) {
+            $endingSpacePos = strlen($matchingWord);
+        }
+        $matchingWord = mb_substr($matchingWord, 0, $endingSpacePos);
+
+        $wrappedMatching = $prefix.$matchingWord.$affix;
+        return str_replace($matchingWord, $wrappedMatching, $input);
+    }
+
+    protected function stripos_array($haystack, $needles) {
+        if (is_array($needles)) {
+            $results = array();
+            foreach ($needles as $str) {
+                if (is_array($str)) {
+                    $pos = $this->stripos_array($haystack, $str);
+                } else {
+                    $pos = mb_stripos($haystack, $str);
+                }
+                if ($pos !== false) {
+                    array_push($results, $pos);
+                }
+            }
+            return ((!empty($results) ? min($results) : 0));
+        } else {
+            return mb_stripos($haystack, $needles);
+        }
     }
 
 }
